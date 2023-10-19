1157e417c8c70b603dd12b930bd48b9f8141e5a1
RZLRZQ <146320340+RZLRZQ@users.noreply.github.com>
2023-10-19 12:02:42 +0700

    pasien dan dokter poliklinik

--- poliklinik/pasien.php
+++ poliklinik/pasien.php
@@ -19,12 +19,12 @@ include_once("koneksi.php");
     integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
     crossorigin="anonymous">   
     
-    <title>Form Pengisian Dokter</title>   <!--Judul Halaman-->
+    <title>Form Pengisian Pasien</title>   <!--Judul Halaman-->
 </head>
 <body>
 <div class="container">
 <h3>
-    Dokter
+    Pasien
     <small class="text-muted">
         Silahkan mengisi.
     </small>
@@ -32,7 +32,7 @@ include_once("koneksi.php");
 <hr>    
 <!--Form Input Data-->
 
-<form class="form row" method="POST" action="" name="myForm" onsubmit="return(validate());">
+<form class="form" method="POST" action="" name="myForm" onsubmit="return(validate());">
     <!-- Kode php untuk menghubungkan form dengan database -->
     <?php
     $nama = '';
@@ -40,10 +40,10 @@ include_once("koneksi.php");
     $no_hp = '';
     if (isset($_GET['id'])) {
         $ambil = mysqli_query($mysqli, 
-        "SELECT * FROM poliklinik 
+        "SELECT * FROM pasien 
         WHERE id='" . $_GET['id'] . "'");
         while ($row = mysqli_fetch_array($ambil)) {
-            $nama = $row['nama'];
+            $nama = $row['datanama'];
             $alamat = $row['alamat'];
             $no_hp = $row['no_hp'];
         }
@@ -89,43 +89,60 @@ include_once("koneksi.php");
         </tr>
     </thead>
     <!--tbody berisi isi tabel sesuai dengan judul atau head-->
+    <?php
+$result = mysqli_query($mysqli, "SELECT * FROM pasien");
+$no = 1;
+while ($data = mysqli_fetch_array($result)) {
+?>
+    <tr>
+        <td><?php echo $no++ ?></td>
+        <td><?php echo $data['datanama'] ?></td>
+        <td><?php echo $data['alamat'] ?></td>
+        <td><?php echo $data['no_hp'] ?></td>
+        <td>
+            <a class="btn btn-success rounded-pill px-3" href="index.php?page=pasien&id=<?php echo $data['id'] ?>">Ubah</a>
+            <a class="btn btn-danger rounded-pill px-3" href="index.php?page=pasien&id=<?php echo $data['id'] ?>&aksi=hapus">Hapus</a>
+        </td>
+    </tr>
+<?php
+}
+?>
 </table>
 <?php
 if (isset($_POST['simpan'])) {
     if (isset($_POST['id'])) {
-        $ubah = mysqli_query($mysqli, "UPDATE poliklinik SET 
-                                        nama = '" . $_POST['nama'] . "',
+        $ubah = mysqli_query($mysqli, "UPDATE pasien SET 
+                                        datanama = '" . $_POST['nama'] . "',
                                         alamat = '" . $_POST['alamat'] . "',
                                         no_hp = '" . $_POST['no_hp'] . "'
                                         WHERE
                                         id = '" . $_POST['id'] . "'");
     } else {
-        $tambah = mysqli_query($mysqli, "INSERT INTO poliklinik(nama,alamat,no_hp) 
+        $tambah = mysqli_query($mysqli, "INSERT INTO pasien(datanama,alamat,no_hp) 
                                         VALUES ( 
                                             '" . $_POST['nama'] . "',
                                             '" . $_POST['alamat'] . "',
-                                            '" . $_POST['no_hp'] . "',
-                                            '0'
+                                            '" . $_POST['no_hp'] . "'
                                             )");
     }
 
     echo "<script> 
-            document.location='index.php';
+            document.location='index.php?page=pasien';
             </script>";
 }
 
 if (isset($_GET['aksi'])) {
     if ($_GET['aksi'] == 'hapus') {
-        $hapus = mysqli_query($mysqli, "DELETE FROM poliklinik WHERE id = '" . $_GET['id'] . "'");
+        $hapus = mysqli_query($mysqli, "DELETE FROM pasien WHERE id = '" . $_GET['id'] . "'");
     } else if ($_GET['aksi'] == 'ubah_status') {
-        $ubah_status = mysqli_query($mysqli, "UPDATE poliklinik SET 
+        $ubah_status = mysqli_query($mysqli, "UPDATE pasien SET 
                                         status = '" . $_GET['status'] . "' 
                                         WHERE
                                         id = '" . $_GET['id'] . "'");
     }
 
     echo "<script> 
-            document.location='index.php';
+            document.location='index.php?page=pasien';
             </script>";
 }
 ?>
